9cd79248ece447db0c230eda61fe1909c6d0f974
Omar <omarcs7r@gmail.com>
2018-04-23 13:53:58 +0200

    [IMP] raya_customize: Añadimo un campo de cant. a mano en los movimiento y cantidad pedida en los informes de albarán

--- project-addons/raya_customize/models/stock_picking.py
+++ project-addons/raya_customize/models/stock_picking.py
@@ -3,6 +3,7 @@
 # For copyright and license notices, see __openerp__.py file in root directory
 ##############################################################################
 from openerp import fields, models
+import openerp.addons.decimal_precision as dp
 
 
 class StockPicking(models.Model):
@@ -11,3 +12,13 @@ class StockPicking(models.Model):
     partner_category_id = fields.Many2many(
         related='partner_id.category_id',
         store=True)
+
+
+class StockMove(models.Model):
+
+    _inherit = "stock.move"
+
+    qty_available = fields.\
+        Float('Qty available', readonly=True,
+              related='product_id.qty_available',
+              digits_compute=dp.get_precision('Product Unit of Measure'))
